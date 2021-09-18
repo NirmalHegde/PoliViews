3cfbca2d3c3d726973b3568a65b1b543e77ce6c7
Your Name <parth.spatel871@gmail.com>
2021-09-18 02:11:29 -0400

    eod

--- backend/app.py
+++ backend/app.py
@@ -2,20 +2,43 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# def queryAnalyzer(query):
 
-@app.route('/', methods=["GET"])
+#     political_info = {
+#         "justin treadeau": "liberal",
+#         "jagmeet singh": "ndp",
+#         "erin o'toole": "conservative",
+#     }
+
+#     justin_keywords = ['justin', 'treadeau', 'justin treadeau', 'justintreadeau ']
+#     jagmeet_keywords = ['jagmeet', 'singh', 'jagmeet singh', 'jagmeetsingh']
+#     _keywords = ['erin', 'toole', "erin o' toole", 'erintoole ']
+
+
+
+
+#     for word in query:
+
+#         if political_info[word] 
+
+@app.route('/api/search/', methods=["GET", "POST"])
 def index():
 
-    return jsonify({"hello": "world"}), 200
+    query = request.args['query'] #what the user searches in the frontend
 
-@app.route('/api/search/', methods=["GET"])
-def index():
+    #body params
+    req_data = request.get_json()
+    party = req_data['Party']
+    candidate = req_data['Candidate']
 
-    query = request.args['query']
+    #json output
+    response = {
+        "query": query,
+        "party": party,
+        "candidate": candidate
+    }
 
-    return jsonify({"query": query}), 200
-
-
+    return jsonify(response), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
